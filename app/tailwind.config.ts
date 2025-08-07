67a4f8b521539e897b586ede0314766150cd57be
Gustavo Mazzo Peluzzo <flipgmp@gmail.com>
2025-08-07 10:07:17 -0300

    chore: change some configuration

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,11 +1,19 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
+
 const config: Config = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    reggae: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        gelo: 'var(--color-gelo)',
+      },
+    },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;
